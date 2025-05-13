b7a3f4ed9713b1dd09c01ab35e649c6a92cdab88
Thomasmp00 <thomaxx00@gmail.com>
2025-05-13 10:57:24 +0200

    Initializes Booking Microservice structure
    Sets up the initial structure for the Booking Microservice, including:
    
    - Booking model with basic properties.
    - IBookingRepository and BookingRepository for data access.
    - IBookingService and BookingService for business logic.
    - BookingController for API endpoints with basic CRUD operations.
    
    Removes the default WeatherForecast controller.

---
+++ Booking-Application/Booking-Microservice/Application/Interfaces/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using Booking_Microservice.Domain.Models;
+
+namespace Booking_Microservice.Application.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<Booking> CreateBooking(Booking booking);
+        Task<IEnumerable<Booking[]>> GetAllBookings();
+        Task<bool> UpdateBookingAsync(Booking booking);
+        Task<bool> DeleteBookingAsync(int id);
+    }
+}
